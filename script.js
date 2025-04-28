950e42fd53dca485b5359db9c5bddda344216343
pmnaing <bryanboyster@gmail.com>
2025-04-28 11:24:59 +1000

    Add computer and human logic

--- script.js
+++ script.js
@@ -1,1 +1,21 @@
-console.log("Hello World!")
+
+
+function getComputerChoice() {
+    let computerChoice = Math.floor(Math.random() * 3 + 1);
+
+    if(computerChoice == 1) {
+        return "rock";
+    }
+    if(computerChoice == 2) {
+        return "paper";
+    }
+    else {
+        return "scissor";
+    }
+}
+
+function getHumanChoice() {
+    let userPrompt = prompt("Pick your sign:\nRock\nPaper\nScissors");
+    return userPrompt.toLowerCase();
+}
+console.log(getHumanChoice())
